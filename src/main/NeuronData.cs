ad809ba0b326d79300ba70db3d73f8cce52f04be
Elmer Roderick Bool <elmer.bool@ei8.works>
2020-03-26 00:48:29 +1100

    Initial commit

---
+++ src/main/NeuronData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace org.neurul.Cortex.Common
+{
+    public class NeuronData
+    {
+        public string Id { get; set; }
+
+        public bool Active { get; set; }
+
+        public int Version { get; set; }
+    }
+}
---
+++ src/main/Types.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace org.neurul.Cortex.Common
+{
+    public enum NeurotransmitterEffect
+    {
+        Inhibit = -1,
+        NotSet = 0,
+        Excite = 1
+    }
+}
